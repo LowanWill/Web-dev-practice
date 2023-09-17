9ba5e7be1177852343ba2e0e15f9eb5dfe451ff4
LowanWill <lowannwil@byui.edu>
2023-09-17 00:03:36 -0600

    correct getdates,js error

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 const d = new Date();
-const lastModified = document.querySelector("#lastUpdated");
+const lastModified = document.querySelector("#lastModified");
 lastModified.innerHTML = `Last Modified: ${new Date().toLocaleString("default", "long")}`;
 const copyright = document.querySelector("#copyright");
 const year = d.getFullYear();
